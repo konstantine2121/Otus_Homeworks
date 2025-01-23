577041a536ad6176c5b417208293fa75adef57f5
konstantine2121 <kach89kach89@gmail.com>
2025-01-23 15:06:48 +0300

    Add SpaceCounter. Add Files to read

---
+++ 2_Parallel_Files_Reading/Parallel_Files_Reading/Parallel_Files_Reading/SpaceCounter.cs
@@ -0,0 +1,35 @@
+﻿namespace Parallel_Files_Reading
+{
+    internal class SpaceCounter
+    {
+        public static async Task<int> Count(string filepath)
+        {
+            const char space = ' ';
+            int total = 0;
+
+            if (string.IsNullOrWhiteSpace(filepath) || !File.Exists(filepath))
+            {
+                return 0;
+            }
+
+            try
+            {
+                var content = await File.ReadAllTextAsync(filepath);
+
+                foreach (char character in content)
+                {
+                    if (character == space)
+                    {
+                        total++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync($"Error on file: '{filepath}'.{Environment.NewLine}Message: {ex.Message}");
+            }
+
+            return total;
+        }
+    }
+}
