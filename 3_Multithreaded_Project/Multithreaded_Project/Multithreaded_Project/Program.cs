bf46d6529bffb9d3d50a180b1fcf92ecb0b9f6aa
konstantine2121 <kach89kach89@gmail.com>
2025-01-23 20:18:14 +0300

    Add empty task 'Multithreaded_Project'

---
+++ 3_Multithreaded_Project/Multithreaded_Project/Multithreaded_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Multithreaded_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
