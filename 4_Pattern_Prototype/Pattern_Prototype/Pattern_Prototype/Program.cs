da86c0e45cfcb65f1734063111b0288033c092dc
konstantine2121 <kach89kach89@gmail.com>
2025-01-30 13:15:57 +0300

    Adds Pattern_Prototype solution

---
+++ 4_Pattern_Prototype/Pattern_Prototype/Pattern_Prototype/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pattern_Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
